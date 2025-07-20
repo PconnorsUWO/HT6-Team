464547c05e0fcd5576226e2cf8f471e1de2fcf48
Hamza Khamissa <hkhamiss@uwo.ca>
2025-07-20 02:02:30 -0400

    testing new landing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Interview from "./pages/interview";
-import TakePhoto from "./pages/take_photo";
+import Interview from './pages/interview';
+import TakePhoto from './pages/take_photo';
+import Final from './pages/Final';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/interview" element={<Interview />} />
           <Route path="/take_photo" element={<TakePhoto />} />
+          <Route path="/final" element={<Final />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
